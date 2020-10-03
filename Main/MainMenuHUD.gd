e3f0de9f560368f809094dcf65bb459c9b201c42
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-10-03 12:10:12 -0400

    change mushroom stalk sizes

--- Main/MainMenuHUD.gd
+++ Main/MainMenuHUD.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+onready var pressed:AudioStreamPlayer = $AudioStreamPlayer
 
 func _on_Button_pressed():
+	pressed.play()
+	yield(get_tree().create_timer(0.5), "timeout")
 	var _error = get_tree().change_scene("res://Main/Main.tscn")
