3be56eb414f168fbfd8e091f340fb923d5a3c654
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-10-03 10:17:42 -0400

    Add enemy

---
+++ Enemy.gd
@@ -0,0 +1,26 @@
+class_name Enemy
+extends Spatial
+
+var primary_rotation:float
+var secondary_rotation:float
+onready var secondary_rotation_point:Spatial = $Spatial
+
+func _ready():
+	randomize()
+	rotation_degrees.y = rand_range(1,360)
+	secondary_rotation_point.rotation_degrees.x = rand_range(1,360)
+	primary_rotation = (randi()%3)-1
+	secondary_rotation = (randi()%3)-1
+
+func _process(delta):
+	var dir := Vector2(primary_rotation, secondary_rotation).normalized()
+	secondary_rotation_point.rotation_degrees.x += dir.y*0.5
+	rotation_degrees.y += dir.x*0.5
+
+func _on_Timer_timeout():
+	primary_rotation = (randi()%3)-1
+	secondary_rotation = (randi()%3)-1
+
+func _on_Enemy_area_entered(area):
+	if area.name == "Player":
+		print("YUMMY PLAYERS")
