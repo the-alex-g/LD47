a5f808ccd9950f3fe5826d8a621c07749c224725
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-10-02 20:57:05 -0400

    Start making player rotate when moved

--- Player.gd
+++ Player.gd
@@ -17,6 +17,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Backward"):
 		velocity.x -= 1
 	velocity = velocity.normalized()*speed*delta
+	var angle := rad2deg(velocity.angle())
+	rotation_degrees = Vector3(0,angle,0)
 	var error = move_and_slide_with_snap(Vector3(velocity.x, 0, velocity.y), Vector3(0,-1,0))
 	if get_global_transform().origin.z >= right_boundary:
 		global_transform.origin.z = left_boundary+1
